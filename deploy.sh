0e7ced17757a7b7af9997c5a56ef1396730508dd
Blake Winton <bwinton@latte.ca>
2017-10-30 13:39:28 -0400

    Tweak the readme to test the build.

--- deploy.sh
+++ deploy.sh
@@ -12,20 +12,14 @@ cd ..
 
 # run our compile script, discussed above
 npm run build
-echo "Ran build."
 
 # inside the gh-pages repo we'll pretend to be a new user
 cd dist
 git config user.name "Travis CI"
 git config user.email "firefox-ux-team@mozilla.com"
 
-ls -al
-echo "$(git status --porcelain)"
-echo "${TRAVIS_PULL_REQUEST}"
-
 if [ -n "$(git status --porcelain)" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   git add -Af .
-  echo "Git adding ${TRAVIS_COMMIT_RANGE}"
   git commit -m "Deploy ${TRAVIS_COMMIT_RANGE} to GitHub Pages."
 
   # Force push from the current repo's master branch to the remote
@@ -33,5 +27,4 @@ if [ -n "$(git status --porcelain)" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; th
   # will be lost, since we are overwriting it.) We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
   git push --force --quiet "https://bwinton:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages
-  echo "Pushing to ${TRAVIS_REPO_SLUG}"
 fi
--- html/README.md
+++ html/README.md
@@ -1,3 +1,3 @@
 # Product Identity Stuff
 
-A place to store new icons and stuff.
+A place to store new icons and logos.
